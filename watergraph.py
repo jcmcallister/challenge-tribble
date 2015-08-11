4308b9481b9681d42bb53ad622a66960f244e375
jcmcallister <jchar.mc@gmail.com>
2015-08-11 18:09:25 -0400

    progress on watergraph, edge cases need work

--- watergraph.py
+++ watergraph.py
@@ -22,51 +22,96 @@
 
 x_interval = 1
 y_interval = 1
-fx = (3,4,5,7,7,3,2,3,4,6,5,1,0,5)
+# fx = (3,4,5,7,7,3,2,3,4,6,5,1,0,5)
+# fx = (5,1,5) # WORKS! 1 valley, should yield 4
+# fx = (5,1,5,3,0,3) # WORKS! multi peaks should yield 9
+# fx = (1,1,4,1,0) # WORKS! just one peak, vol == 0
+# fx = (1,1) # WORKS! vol == 0
+#fx = (1,2,3,0,3,1,0) # WORKS! mountain with a crater on top, vol == 3
+# fx = (1,2,3,0,3,1,0,5) # max at END, 2 valleys, vol should be 8
+fx = (3,1,2) # 1 valley, max at position 0, vol == 1
+# fx = (2,1,3) # WORKS! 1 valley, max at end, vol == 1
+
 peaks = []
-lastpeaklocation = 0
-currMax = -999
 i=0
+currMax = -1
+waterVolume = 0
 
 while i < len(fx):
-	print "fx[%d]\t: %d" % (i, fx[i])
+	# print "fx[%d]\t: %d" % (i, fx[i])
 
-	if currMax < fx[i]:
-		currMax = fx[i];
-
-	# get a peak list
-	if lastpeaklocation == 0:
-		# leftpeaks.append(fx[i]);
-		leftpeak = fx[i]
+	# update max
+	if fx[i] > currMax:
+		currMax = fx[i]
+		# if we update max, we should also update the volume between all spaces
+		# 	where there is growth (after a peak -> shrink)
 
 	# if we can peek at next
 	if i < (len(fx) - 1):
 
 		# if next is growing
-		if fx[i+1] > fx[i]:
-			print "\tGROW from %d to %d!" % (fx[i], fx[i+1])
-			lastpeaklocation = i+1;
-		
-			# we reset the peak to the next one, which is higher
-			leftpeak = fx[i+1]
+		if fx[i+1] >= fx[i]:
+			# print "\tGROW from %d to %d!" % (fx[i], fx[i+1])
+			print 'g'
 
-			# were we in a valley just now? if so, add locations to the valley list
-
-
+			# if we're growing out of a slump!
+			if currMax >= 0:
+				waterVolume += currMax - (y_interval * fx[i])
 
 		elif fx[i+1] < fx[i]:
 
-			# next is shrinking!
+			# next is sinking!
 
-			print "\tSHRINK from %d to %d!" % (fx[i], fx[i+1])
-
-			# was there a leftpeak to match this peak? if so, calc the water depth
-
-			lastpeaklocation = i;
-
-		peaks.append(lastpeaklocation)
-
+			# print "\tSINK from %d to %d!" % (fx[i], fx[i+1])
+			peaks.append(i)
+	else:
+		# in the last case that the max is the LAST elem
+		if currMax == fx[i]:
+			waterVolume += currMax - (y_interval * fx[i])
 	i += 1;
 
-print "the max of the values is %d" % currMax
+# print "the max of the values is %d" % currMax
 print peaks
+
+print "water volume is %d" % waterVolume
+
+# get min of j & j+1, calculate volume of all f[x] between these indices & add to total
+# waterVolume = 0
+# maxpeak = -1
+# minvalley = 2**16
+# for j in peaks:
+# 	curr = fx[j]
+# 	nxt = fx[j+1]
+
+# 	if curr > maxpeak:
+# 		maxpeak = curr
+
+
+# 	if curr < minvalley:
+# 		minvalley = curr
+
+	#look at all indices between maxpeak and minvalley
+
+
+
+"""
+
+breakdown
+
+	approach 1: growth-based
+		get next # see if we're still growing
+		if next < curr, curr is a peak that is now descending
+		record curr as a peak in a list
+
+
+		DIVERGING IDEAS
+			1: only record the peaks, then do another pass to get the depths
+
+			2: calculate the a total volume per iteration based on growth/depth
+
+		Idea 1 sounds like it would be easier for devs to follow, costing us 1*O(n)
+			no big deal, i guess
+
+		what if instead we had a list of 
+
+"""
